aa2bbf0dfee2adf2aa9296509a73ea8cb43d0050
Bakhtawar-Baloch <balochbakh@gmail.com>
2026-06-19 21:43:01 +0500

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -342,38 +342,3 @@ function loadPayments() {
 }
 
 loadPayments();
-
-
-
-
-const serviceType =
-    document.getElementById("serviceType").value;
-
-services: [
-   {
-      category: "SALON",
-      name: "Haircut"
-   },
-
-   {
-      category: "SALON",
-      name: "Facial Treatment"
-   },
-
-  {
-      category: "SPA",
-      name: "Red Light Therapy"
-   },
-
-
-   {
-      category: "SPA",
-      name: "Sauna"
-   },
-
-  {
-      category: "GYM",
-      name: "Gym Subscription - Quarterly"
-   },
-
-]
